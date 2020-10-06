7f41a416b55575bee099a802cd7948b37399e79b
NoeDiaz2020 <71098248+NoeDiaz2020@users.noreply.github.com>
2020-10-06 11:53:43 -0500

    Delete Corrida.java

--- interfaceview/Corrida.java
+++
@@ -1,9 +0,0 @@
-package interfaceview;
-public class Corrida {
-    public static void main(String[] args) {
-        Vista v = new Vista();
-        Modelo m = new Modelo(v);
-        Controlador c = new Controlador(m, v);
-        c.run();
-    }
-}
